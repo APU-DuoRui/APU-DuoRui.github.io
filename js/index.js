07cfcf80d5cb8af655b910a76b8074b6efb21c22
3128125949@qq.com <3128125949@qq.com>
2020-07-15 16:18:11 +0800

    音乐完成一半

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
 $(function () {
+    //(1)是给触摸轮播图注册animate
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: 3000,
+    });
     //1.创建一个定时器 发送ajax请求
     function setBigPic() {
         $.ajax({
@@ -19,30 +24,33 @@ $(function () {
     setBigPic();
     // 每隔半小时换一张. 
     setInterval(setBigPic, 1000 * 60 * 30);
-    //2.需求分析  当页面滚动10px 让nav改变为固定定位
-    /*  $(window).on('scroll', function () {
-         //2.1判断scrollTop值
-         if ($(this).scrollTop() > $('.nav').height()) {
-             //2.2修改nav固定定位
-             $('.nav').css({
-                 position: "fixed",
-                 top: '0',
-                 // left: '10%',
-              
-             })
-             //2.3.设置details的marginTop ，避免脱标导致顿闪
-             $('.details').css({
-                 marginTop: $('.nav').height() + 10 + 'px'
-             })
-         } else {
-             //2.4.如果用户返回顶部就修改nav为静态定位
-             $('.nav').css({
-                 position: "static",
-             });
-             //2.5.设置details的marginTop为默认值
-             $('.details').css({
-                 marginTop: "-3px"
-             })
-         }
-     }) */
-})
+    //2.创建一个定时器来控制背景图片隐藏
+    setInterval(function () {
+        $('#loading').hide();
+        $('.main').show();
+    }, 3000);
+
+    $('.btn').on('click', function () {
+        $('.current').show();
+        $('.main').hide();
+    })
+    $('.x').on('click', function () {
+        $('.current').hide();
+        $('.main').show();
+
+    })
+    /* 1.2 发送天气天气请求
+     * 请求地址：http://wthrcdn.etouch.cn/weather_mini
+     * 请求方法：get
+     * 请求参数：city
+     * 示例： http://wthrcdn.etouch.cn/weather_mini?city=深圳
+     */
+    /*
+       获取当前的地址
+        var ip = "124.127.108.133";
+                    var url = "http://ip.taobao.com/service/getIpInfo.php?ip=" + ip;
+                    $.getJSON(url, function (json) {
+                        alert("您所在的城市是：" + json.data.area + json.data.region);
+                    });
+         */
+});
